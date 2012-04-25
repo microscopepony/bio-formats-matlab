9faeccc62e14fd8a9f012bd6cb19fc73747a4ff8
Sebastien Besson <seb.besson@gmail.com>
2012-04-25 09:25:01 +0100

    Move Matlab functions/tests to their own folder

---
+++ components/bio-formats/matlab/bfopen.m
@@ -0,0 +1,204 @@
+function [result] = bfopen(id)
+% A script for opening microscopy images in MATLAB using Bio-Formats.
+%
+% The function returns a list of image series; i.e., a cell array of cell
+% arrays of (matrix, label) pairs, with each matrix representing a single
+% image plane, and each inner list of matrices representing an image
+% series. See below for examples of usage.
+%
+% Portions of this code were adapted from:
+% http://www.mathworks.com/support/solutions/en/data/1-2WPAYR/
+%
+% This method is ~1.5x-2.5x slower than Bio-Formats's command line
+% showinf tool (MATLAB 7.0.4.365 R14 SP2 vs. java 1.6.0_20),
+% due to overhead from copying arrays.
+%
+% Thanks to all who offered suggestions and improvements:
+%     * Ville Rantanen
+%     * Brett Shoelson
+%     * Martin Offterdinger
+%     * Tony Collins
+%     * Cris Luengo
+%     * Arnon Lieber
+%     * Jimmy Fong
+%
+% NB: Internet Explorer sometimes erroneously renames the Bio-Formats library
+%     to loci_tools.zip. If this happens, rename it back to loci_tools.jar.
+%
+% Here are some examples of accessing data using the bfopen function:
+%
+%     % read the data using Bio-Formats
+%     data = bfopen('C:/data/experiment.lif');
+%
+%     % unwrap some specific image planes from the result
+%     numSeries = size(data, 1);
+%     series1 = data{1, 1};
+%     series2 = data{2, 1};
+%     series3 = data{3, 1};
+%     metadataList = data{1, 2};
+%     % ...etc.
+%     series1_numPlanes = size(series1, 1);
+%     series1_plane1 = series1{1, 1};
+%     series1_label1 = series1{1, 2};
+%     series1_plane2 = series1{2, 1};
+%     series1_label2 = series1{2, 2};
+%     series1_plane3 = series1{3, 1};
+%     series1_label3 = series1{3, 2};
+%     % ...etc.
+%
+%     % plot the 1st series's 1st image plane in a new figure
+%     series1_colorMaps = data{1, 3};
+%     figure('Name', series1_label1);
+%     if isempty(series1_colorMaps{1})
+%         colormap(gray);
+%     else
+%         colormap(series1_colorMaps{1});
+%     end
+%     imagesc(series1_plane1);
+%
+%     % Or if you have the image processing toolbox, you could use:
+%     % imshow(series1_plane1, []);
+%
+%     % Or animate as a movie (assumes 8-bit unsigned data)
+%     v = linspace(0, 1, 256)';
+%     cmap = [v v v];
+%     for p = 1:series1_numPlanes
+%         M(p) = im2frame(uint8(series1{p, 1}), cmap);
+%     end
+%     movie(M);
+%
+%     % Query some metadata fields (keys are format-dependent)
+%     subject = metadataList.get('Subject');
+%     title = metadataList.get('Title');
+
+% -- Configuration - customize this section to your liking --
+
+% Toggle the autoloadBioFormats flag to control automatic loading
+% of the Bio-Formats library using the javaaddpath command.
+%
+% For static loading, you can add the library to MATLAB's class path:
+%     1. Type "edit classpath.txt" at the MATLAB prompt.
+%     2. Go to the end of the file, and add the path to your JAR file
+%        (e.g., C:/Program Files/MATLAB/work/loci_tools.jar).
+%     3. Save the file and restart MATLAB.
+%
+% There are advantages to using the static approach over javaaddpath:
+%     1. If you use bfopen within a loop, it saves on overhead
+%        to avoid calling the javaaddpath command repeatedly.
+%     2. Calling 'javaaddpath' may erase certain global parameters.
+autoloadBioFormats = 1;
+
+% Toggle the stitchFiles flag to control grouping of similarly
+% named files into a single dataset based on file numbering.
+stitchFiles = 0;
+
+% To work with compressed Evotec Flex, fill in your LuraWave license code.
+%lurawaveLicense = 'xxxxxx-xxxxxxx';
+
+% -- Main function - no need to edit anything past this point --
+
+% load the Bio-Formats library into the MATLAB environment
+status = bfCheckJavaPath(autoloadBioFormats);
+assert(status,['Missing Bio-Formats library. Either add loci_tools.jar '...
+    'to the static Java path or add it to the Matlab path.']);
+
+% Prompt for a file if not input
+if nargin==0 || exist(id,'file') == 0
+  [file,path] = uigetfile(bfGetFileExtensions, 'Choose a file to open');
+  id = [path file];
+  if isequal(path,0) || isequal(file,0), return; end
+end
+
+% initialize logging
+loci.common.DebugTools.enableLogging('INFO');
+
+% Get the channel filler
+r=bfGetReader(id,stitchFiles);
+
+numSeries = r.getSeriesCount();
+result = cell(numSeries, 2);
+for s = 1:numSeries
+    fprintf('Reading series #%d', s);
+    r.setSeries(s - 1);
+    pixelType = r.getPixelType();
+    bpp = loci.formats.FormatTools.getBytesPerPixel(pixelType);
+    bppMax = power(2, bpp * 8);
+    numImages = r.getImageCount();
+    imageList = cell(numImages, 2);
+    colorMaps = cell(numImages);
+    for i = 1:numImages
+        if mod(i, 72) == 1
+            fprintf('\n    ');
+        end
+        fprintf('.');
+        arr=bfGetPlane(r,i);
+
+        % retrieve color map data
+        if bpp == 1
+            colorMaps{s, i} = r.get8BitLookupTable()';
+        else
+            colorMaps{s, i} = r.get16BitLookupTable()';
+        end
+        
+        warning off
+        if ~isempty(colorMaps{s, i})
+            newMap = colorMaps{s, i};
+            m = newMap < 0;
+            newMap(m) = newMap(m) + bppMax;
+            colorMaps{s, i} = newMap / (bppMax - 1);
+        end
+        warning on
+
+
+        % build an informative title for our figure
+        label = id;
+        if numSeries > 1
+            qs = int2str(s);
+            label = [label, '; series ', qs, '/', int2str(numSeries)];
+        end
+        if numImages > 1
+            qi = int2str(i);
+            label = [label, '; plane ', qi, '/', int2str(numImages)];
+            if r.isOrderCertain()
+                lz = 'Z';
+                lc = 'C';
+                lt = 'T';
+            else
+                lz = 'Z?';
+                lc = 'C?';
+                lt = 'T?';
+            end
+            zct = r.getZCTCoords(i - 1);
+            sizeZ = r.getSizeZ();
+            if sizeZ > 1
+                qz = int2str(zct(1) + 1);
+                label = [label, '; ', lz, '=', qz, '/', int2str(sizeZ)];
+            end
+            sizeC = r.getSizeC();
+            if sizeC > 1
+                qc = int2str(zct(2) + 1);
+                label = [label, '; ', lc, '=', qc, '/', int2str(sizeC)];
+            end
+            sizeT = r.getSizeT();
+            if sizeT > 1
+                qt = int2str(zct(3) + 1);
+                label = [label, '; ', lt, '=', qt, '/', int2str(sizeT)];
+            end
+        end
+
+        % save image plane and label into the list
+        imageList{i, 1} = arr;
+        imageList{i, 2} = label;
+    end
+
+    % extract metadata table for this series
+    metadataList = r.getMetadata();
+
+    % save images and metadata into our master series list
+    result{s, 1} = imageList;
+    result{s, 2} = metadataList;
+    result{s, 3} = colorMaps;
+    result{s, 4} = r.getMetadataStore();
+    fprintf('\n');
+end
+r.close();
